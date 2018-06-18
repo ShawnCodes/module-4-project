efe43917b179c93b075ca117ddeb8a1325806d72
github username <github email address>
2018-06-18 13:49:32 -0400

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  constructor() {
+    super(props);
+
+  }
+
+  render {
+    return (
+      <div className="header">HEADER</div>
+    )
+  }
+}
