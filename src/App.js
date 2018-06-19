f4a7d04f82f52da37a355ac82d88d4bdc4f86ddd
github username <github email address>
2018-06-19 10:43:11 -0400

    main content rendered from api

--- src/App.js
+++ src/App.js
@@ -3,34 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import Content from './components/content';
 import Header from './components/header';
+const URL = 'http://localhost:10524/api/v1/items'
 
 class App extends Component {
-  constructor(){
-    super()
-
-    this.state={
-      value: ''
-    }
+  state = {
+    items: []
   }
 
-  componentDidMount(){
-    fetch('http://localhost:10524/api/v1/items')
-    .then(res => res.json())
-    .then(res => console.log(res))
-  }
 
-  render() {
-    return (
-      <div className="container">
-      <div className="header">
+componentDidMount() {
+  fetch(URL).then(res => res.json()).then(json => this.setState({items: json}))
+}
+
+render() {
+  return (
+    <div className="container">
+    <div className="header">
       <Header/>
-  </div>
-        <div className="content">
-          <Content />
-      </div>
-      </div>
-            );
-  }
+    </div>
+    <div className="content">
+      <Content contents={this.state.items}/>
+    </div>
+  </div>);
+}
 }
 
 export default App;
--- src/components/content.js
+++ src/components/content.js
@@ -4,27 +4,23 @@ import React, { Component } from 'react';
 class Content extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      items:[]
-    }
   }
   render() {
+    console.log(this.props);
     return (
       <div>
-      {/* <div>
-      {this.props.items.map((item, index) =>
-      <div onClick={this.props.someFunction}>
-      <div class="image">
-      <img src={item.img} />
-      </div>
-      <div class="content" key={index}>
-      <a class="header">{item.name}</a>
-      <div class="description">
-      {item['description']}
+      <div>
+      {this.props.contents.map((content, index) =>
+      <div>
+      <img id="images" src={content.img_src} />
+    <div className="content" key={content.id}>
+      <a className="header">{content.name}</a>
+    <div className="description">
+      {content['description']}
       </div>
       </div>
       </div>)}
-      </div> */}
+      </div>
       </div>
     );
   }
