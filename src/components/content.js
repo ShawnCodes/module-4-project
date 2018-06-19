37f189439b244f1f2bc4004d4328c123b69f4723
github username <github email address>
2018-06-19 01:49:51 -0400

    stylying, data mapping

--- src/components/content.js
+++ src/components/content.js
@@ -5,12 +5,27 @@ class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      items:[]
     }
   }
   render() {
     return (
-      <div>CONTENT</div>
+      <div>
+      {/* <div>
+      {this.props.items.map((item, index) =>
+      <div onClick={this.props.someFunction}>
+      <div class="image">
+      <img src={item.img} />
+      </div>
+      <div class="content" key={index}>
+      <a class="header">{item.name}</a>
+      <div class="description">
+      {item['description']}
+      </div>
+      </div>
+      </div>)}
+      </div> */}
+      </div>
     );
   }
 }
