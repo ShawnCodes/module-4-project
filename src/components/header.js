a6ad732f0edd4c406b997fefdeba2124017658eb
AustinPaley <austinpaley32@gmail.com>
2018-06-22 10:34:57 -0400

    Fixed Bug

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,14 @@ import Search from './search'
 
 class Header extends Component {
   render() {
-    console.log(this.props, "header")
+    console.log(this.props.history)
     return (
       <div className="topnav">
       <Link to={'/'} id="home-button" href="#home">Home</Link>
       <a>My Items</a>
       <a>My Wishlist</a>
       <a>Categories</a>
-      {this.props.currentUserId === "" && this.props.currentUserName == "" ? <Link to={'/register'} id="registration-button">Register</Link> : <Link to={'/login'} id="login-button">Login</Link>}
+      {this.props.currentUserId === "" && this.props.currentUserName === "" ? <Link to={'/register'} id="registration-button">Register</Link> : <Link to={'/login'} id="login-button">Login</Link>}
       <Search handleSearch={this.props.handleSearch}/>
       </div>
     );
